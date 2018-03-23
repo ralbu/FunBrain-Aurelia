7ad293681f9847acc9e085cbe8ad4b763b584342
Ruslan <ruslan.albu@hotmail.com>
2018-03-23 14:25:36 +0000

    fetch client

--- FunBrain/src/users/user-service.ts
+++ FunBrain/src/users/user-service.ts
@@ -1,6 +1,17 @@
+import {HttpClient} from 'aurelia-fetch-client';
+
 export default class UserService {
 
+  httpClient: HttpClient;
+  constructor(httpClient: HttpClient){
+    this.httpClient = httpClient.configure(c => c.withBaseUrl('http://localhost:64884/api'));
+
+  }
+
   public getUsers(){
+    this.httpClient.fetch('users')
+      .then(response => console.log('resp: ', response));
+    
     return ['user 1', 'user 2'];
   }
 }
