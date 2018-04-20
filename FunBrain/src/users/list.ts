f6a2b73edab29d6c9a848adae4b90344800311b5
Ruslan <ruslan.albu@hotmail.com>
2018-04-20 14:00:11 +0100

    modal dialog

--- FunBrain/src/users/list.ts
+++ FunBrain/src/users/list.ts
@@ -8,11 +8,27 @@ export class UserList {
   private userService: UserService;
   private users: UserModel[];
 
+  cl = "visible active";
+  cl2 = "visible active"
+  private isModal: boolean;
+
   constructor(userService: UserService) {
     this.userService = userService
   }
 
   async activate() {
     this.users = await this.userService.getUsers();
+  }
+
+  delete() {
+    this.isModal = true;
+
+    console.log('Delte');
+    let modal = this.deleteModalParent;
+    console.log('modal', modal);
+  }
+
+  closeDialog(){
+    this.isModal = false;
   }
 }
